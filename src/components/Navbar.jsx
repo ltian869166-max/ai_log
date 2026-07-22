8070bad87ee1b6c82023f6acb63cbcde715059a1
林甜 <ltian869166@gmail.com>
2026-07-22 14:57:25 +0800

    style: 去掉导航栏Logo中的人物图标

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,12 +46,6 @@ export default function Navbar() {
       <div className="container">
         <div className="navbar-left">
           <a href="#hero" className="navbar-logo">
-            <span className="navbar-avatar">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-                <circle cx="12" cy="7" r="4" />
-              </svg>
-            </span>
             <span className="navbar-brand">LinT</span>
           </a>
         </div>
